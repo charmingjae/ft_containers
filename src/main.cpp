23c2b36b57438bfc4c91d86d1b6b6bfa1217a842
charmingjae <beingeskimo@gmail.com>
2022-08-01 20:59:30 +0900

    feat: vector

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,32 @@
 /*   By: mcha <mcha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:45:43 by mcha              #+#    #+#             */
-/*   Updated: 2022/07/26 17:29:09 by mcha             ###   ########.fr       */
+/*   Updated: 2022/08/01 16:09:35 by mcha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <iterator.hpp>
 #include <iterator>
 #include <vector.hpp>
 #include <vector>
+
 int main(void) {
-    std::vector<int> a;
+    // {
+    //     std::vector<int> a;
+    //     a.push_back(1);
+    //     a.push_back(2);
+    //     a.push_back(3);
+    //     // a.push_back(4);
+
+    //     std::cout << "capacity : " << a.capacity() << std::endl;
+    //     std::cout << "size : " << a.size() << std::endl;
+    //     a.clear();
+    //     std::cout << "clear capacity : " << a.capacity() << std::endl;
+    //     std::cout << "clear size : " << a.size() << std::endl;
+    // }
+    { ft::__vector_base<int> b;
+	}
+    // system("leaks container");
     return (0);
 }
