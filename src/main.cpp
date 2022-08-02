c4ee8e7ddf52af8b219d8fb6beaf3fab6d564f29
charmingjae <beingeskimo@gmail.com>
2022-08-02 22:47:59 +0900

    feat: rollback

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcha <mcha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:45:43 by mcha              #+#    #+#             */
-/*   Updated: 2022/08/02 22:01:06 by mcha             ###   ########.fr       */
+/*   Updated: 2022/08/02 22:21:11 by mcha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ int main(void)
 	ft::vector<int> a(7);
 	std::cout << a.size() << std::endl;
 	std::cout << a.capacity() << std::endl;
+
+	ft::vector<int> b(a);
 	return (0);
 }
